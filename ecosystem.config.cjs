f26854d25bf633d2de116882dae8d7a6160bc7a5
flamousz <valflamous@gmail.com>
2023-06-13 01:07:34 +0700

    fixing ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,17 @@
 module.exports = {
   apps : [{
     name   : "app1",
-    script : "./app.js"
-  }],
-  env: {
+    script : "./app.js",
+    env: {
         // Dibutuhkan untuk nodejs menganggap bahwa
         // kode berjalan pada production
         NODE_ENV: "production",
-	JWT_PASSWORD: "Darkkamban12"
+        JWT_PASSWORD: "Darkkamban12",
         // Dibutuhkan pada aplikasi app.js untuk
         // set port aplikasi
-        PORT: 80,
-      },
+        PORT: 80
+      }
+  }]
 }
+
+
